d2211127e367329275b04c08cd60ada3345813b7
Silas Schwarz <silas.schwarz@moovel.com>
2020-03-30 16:24:35 +0200

    Initial commit

---
+++ src/main/kotlin/sample/App.kt
@@ -0,0 +1,11 @@
+package sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
